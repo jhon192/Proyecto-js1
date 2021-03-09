0e1198c4a9707fa59b7798bed481b1ba27468c43
jhon192 <jhon19.2@outlook.com>
2021-03-09 10:05:45 -0400

    first

---
+++ main.js
@@ -0,0 +1,60 @@
+//Method Get
+/* fetch('https://reqres.in/api/users?page=2')
+.then(res => res.json())
+.then(data => console.log(data.data))
+.catch(e => console.log(e));*/
+
+//Method post
+/* const peticion = fetch('https://reqres.in/api/users',{
+    method: 'POST',
+    body: JSON.stringify({
+        "name": "morpheus",
+        "job": "leader"
+    }),
+    headers:{
+        "Content-Type": "application/json"
+    },
+    credentials: "same-origin"
+}).then(res =>{
+    console.log(res.json())
+})*/
+
+//method Patch/Put
+/* const peticion = fetch('https://reqres.in/api/users',{
+    method: 'PATCH',
+    body: JSON.stringify({
+        "name": "Jhon",
+        "job": "zion resident"
+    }),
+    headers:{
+        "Content-Type": "application/json"
+    },
+    credentials: "same-origin"
+}).then(res =>{
+    console.log(res.json())
+})*/
+
+//axios get default
+/* axios('https://reqres.in/api/users?page=2')
+.then(res => console.log(res.data.data))
+.catch(e => console.log(e));*/
+
+//axios post
+/*axios.post('https://reqres.in/api/users', {
+    "name": "morpheus",
+    "job": "leader"
+}).then(res =>{
+    console.log(res);
+}).catch(e =>{
+    console.log(e);
+})*/
+
+//axios patch/put
+/*axios.patch('https://reqres.in/api/users', {
+    "name": "Jhon",
+    "job": "leader"
+}).then(res =>{
+    console.log(res);
+}).catch(e =>{
+    console.log(e);
+})*/
